30ef47c05a998a2a4c366ca9012f949e9ddfc118
Bradley Walby <intrixthename@gmail.com>
2024-04-14 13:41:34 -0400

    Bug fixing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,43 +1,41 @@
 package org.example;
 
 import net.bytebuddy.build.Plugin;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     private WebDriver Disk_Status;// intialize the webdriver load page.
+
     @BeforeTest
-
     void Unit_0() throws InterruptedException {
-    //check if the browser Window can load (depending on where it should run)
-     WebDriver Disk_Status = new FirefoxDriver();
+        //check if the browser Window can load (depending on where it should run)
+        WebDriver Disk_Status = new FirefoxDriver();
 
-     //Test if the Browser opens by getting the specific link to test if the browser opens.
+        //Test if the Browser opens by getting the specific link to test if the browser opens.
         // In this specfic case we'll do firefox
-     Disk_Status.get("http://localhost:3000");
-    // Provide a small delay to test the browser opening
-     Thread.sleep(1250);
-     // Make it do something here
+        Disk_Status.get("https://www.google.com");
+        // Provide a small delay to test the browser opening
+        Thread.sleep(1250);
+        // Make it do something here
         // depending on th site I'll most likely scroll
         //close the tab
-
-
 
 
 //Testing Endless disk
         Disk_Status = new FirefoxDriver();
         //Open the tab again
-        Disk_Status.get("");
+        Disk_Status.get("http://localhost:3000");
         //check the connection of the site works if it does work it doesn't throw a fail for the site
         //If it doesn't it does not open and the test fails.
         Thread.sleep(1250);
@@ -52,7 +50,7 @@ public class Main {
 
         String pageSource = Disk_Status.getPageSource(); //Initalize the variable of getting a page source.
         // but if the page source contains a specific item,
-        pageSource.contentEquals('');//print the source data and return true
+        pageSource.contentEquals("");//print the source data and return true
         //print the source out here
         Thread.sleep(1250);
         System.out.println(pageSource);
@@ -65,7 +63,7 @@ public class Main {
     }
 
     public class unit1 {
-        public static WebDriver press_disc_play(WebDriver driver, String url, String play_id) {
+        public WebDriver press_disc_play(WebDriver driver, String url, String play_id) throws InterruptedException {
             driver.get(url);
             wait(2000);
             WebElement play_button = driver.findElement(By.id(play_id));
@@ -74,64 +72,65 @@ public class Main {
             return driver;
         }
 
-    void Unit_2() throws InterruptedException {
-        //Testing all the buttons and scrolling if viable
-        // First open a new driver and get the site
+        void Unit_2() throws InterruptedException {
+            //Testing all the buttons and scrolling if viable
+            // First open a new driver and get the site
 
-        WebDriver Disk_Status = new FirefoxDriver();
+            WebDriver Disk_Status = new FirefoxDriver();
 
-        Disk_Status.get("");
-        // Provide a small delay to test the browser opening
-        Thread.sleep(1250);
+            Disk_Status.get("");
+            // Provide a small delay to test the browser opening
+            Thread.sleep(1250);
 // Find the button to select
-        WebElement Nav_pg_1 = Disk_Status.findElement(By.id(""));
-    //Click it
-        Nav_pg_1.click();
-        Thread.sleep(2250);
+            WebElement Nav_pg_1 = Disk_Status.findElement(By.id(""));
+            //Click it
+            Nav_pg_1.click();
+            Thread.sleep(2250);
 
 
-        // Find the button to select
-        WebElement Nav_pg_2 = Disk_Status.findElement(By.id(""));
-        //Click it
-        Nav_pg_2.click();
-        Thread.sleep(2250);
+            // Find the button to select
+            WebElement Nav_pg_2 = Disk_Status.findElement(By.id(""));
+            //Click it
+            Nav_pg_2.click();
+            Thread.sleep(2250);
 
-        // Find the button to select
-        WebElement Nav_pg_3 = Disk_Status.findElement(By.id(""));
-        //Click it
-        Nav_pg_3.click();
-        Thread.sleep(2250);
+            // Find the button to select
+            WebElement Nav_pg_3 = Disk_Status.findElement(By.id(""));
+            //Click it
+            Nav_pg_3.click();
+            Thread.sleep(2250);
 
-        // Find the button to select
-        WebElement Nav_pg_4 = Disk_Status.findElement(By.id(""));
-        //Click it
-        Nav_pg_4.click();
+            // Find the button to select
+            WebElement Nav_pg_4 = Disk_Status.findElement(By.id(""));
+            //Click it
+            Nav_pg_4.click();
 
-        // Find the button to select
-        WebElement Nav_pg_5 = Disk_Status.findElement(By.id(""));
-        //Click it
-        Nav_pg_5.click();
-        // Testing the access of the github repo and issue release
+            // Find the button to select
+            WebElement Nav_pg_5 = Disk_Status.findElement(By.id(""));
+            //Click it
+            Nav_pg_5.click();
+            // Testing the access of the github repo and issue release
 
-        //Navigate to  the github
-        //Nav_pg_#.click();
-        Thread.sleep(1250);
-        //Once loaded scroll on the page
-        //
-        JavascriptExecutor Scrolldown_gh = (JavascriptExecutor) Disk_Status;
-
-
-        //close tab
+            //Navigate to  the github
+            //Nav_pg_#.click();
+            Thread.sleep(1250);
+            //Once loaded scroll on the page
+            //
+            JavascriptExecutor Scrolldown_gh = (JavascriptExecutor) Disk_Status;
 
 
-        //Open the info release
+            //close tab
 
-        // close window
 
-    }
+            //Open the info release
 
-    void Unit_5(){
+            // close window
 
+        }
+
+        void Unit_5() {
+
+        }
     }
 }
 
@@ -155,12 +154,12 @@ class Unit_3 {
         driver.findElement(By.id("username")).sendKeys("IntrixTheName");
         driver.findElement(By.id("password")).sendKeys("HelloWorld");
         Thread.sleep(500);
-        driver.findElement(By.id("login")).click();
+        driver.findElement(By.id("login-button")).click();
     }
 
 
     @Test(priority = 1)
-    void addNewStation() { //Req 3.1
+    void addNewStation() throws InterruptedException { //Req 3.1
         try {
             //Getting any page assumes session login first
             driver.get("http://localhost:3000/radio"); //Nav to radio page
@@ -171,14 +170,14 @@ class Unit_3 {
             driver.findElement(By.id("submit")).click(); //And submit the form
             Thread.sleep(1000);
             driver.navigate().refresh(); //Refresh page
-            Assert.assertTrue(driver.findElements(By.link_text("Radio 538 Ibiza")) != []); //And confirm the new station is present
-        } catch {
-            Assert.assertTrue(false); //If error, assert false for report purpose
+            Assert.assertFalse(driver.findElements(By.linkText("Radio 538 Ibiza")).isEmpty()); //And confirm the new station is present
+        } catch(Exception e) {
+            Assert.fail(); //If error, assert false for report purpose
         }
     }
 
     @Test(priority = 2)
-    void playRadio() {
+    void playRadio() throws InterruptedException {
         //Note: This combines 3.4 & 3.5 in a single case
         try {
             driver.get("http://localhost:3000/radio");
@@ -186,8 +185,8 @@ class Unit_3 {
             Thread.sleep(3000); //Await to ensure audio plays
             driver.findElement(By.className("audio-control")).click(); //And pause again
             Assert.assertTrue(true);
-        } catch {
-            Assert.assertTrue(false);
+        } catch(Exception e) {
+            Assert.fail();
         }
     }
 
@@ -198,9 +197,9 @@ class Unit_3 {
             driver.findElement(By.id("radio-player")).click();
             driver.findElement(By.id("title")).sendKeys("Radio 538 - Ibiza");
             driver.findElement(By.id("submit")).click();
-            Assert.assertEqual(driver.findElement(By.id("title")).getText(), "Radio 538 - Ibiza");
-        } catch {
-            Assert.assertTrue(false);
+            Assert.assertEquals(driver.findElement(By.id("title")).getText(), "Radio 538 - Ibiza");
+        } catch(Exception e) {
+            Assert.fail();
         }
     }
 
@@ -210,11 +209,11 @@ class Unit_3 {
             driver.get("http://localhost:3000/radio");
             driver.findElement(By.id("remove")).click(); //Click on the first radio
             String confirm_box = driver.switchTo().alert().getText(); //Confirm the popup 
-            Assert.assertEqual(confirm_box, "Delete radio, are you sure?");
-            driver.switchTo().alert().click();
-            Assert.assertFalse(driver.findElements(By.id("remove"))) //Assert empty array as falsy
-        } catch {
-            Assert.assertTrue(false);
+            Assert.assertEquals(confirm_box, "Delete radio, are you sure?");
+            driver.switchTo().alert().accept();
+            Assert.assertTrue(driver.findElements(By.id("remove")).isEmpty()); //Assert empty array as falsy
+        } catch(Exception e) {
+            Assert.fail();
         }
     }
 
@@ -246,15 +245,15 @@ class Unit_6 {
     void showsMusic() { //Req 6.1
         try {
             driver.get("http://localhost:3000/library");
-            driver.findElement(By.literalText("Sample Album")).click(); //Click on Sample Album to grab songs
-            Assert.assertTrue(driver.findElements(By.className("audio-player")).length() == 3) // Assert 3 songs found
-        } catch {
-            Assert.assertTrue(false);
+            driver.findElement(By.linkText("Sample Album")).click(); //Click on Sample Album to grab songs
+            Assert.assertTrue(driver.findElements(By.className("audio-player")).size() == 3); // Assert 3 songs found
+        } catch(Exception e) {
+            Assert.fail();
         }
     }
 
     @Test(priority = 2)
-    void startSong() { //Req 6.3 w/ sprinkles (and requires manual observation for verification)
+    void startSong() throws InterruptedException { //Req 6.3 w/ sprinkles (and requires manual observation for verification)
         try {
             driver.get("http://localhost:3000/library");
             driver.findElement(By.id("play-pause")).click(); //Click play
@@ -264,24 +263,24 @@ class Unit_6 {
             driver.findElement(By.id("rwnd")).click(); //Click reqind (seek -15s)
             Thread.sleep(3000);
             Assert.assertTrue(true);
-        } catch {
-            Assert.assertTrue(false);
+        } catch(Exception e) {
+            Assert.fail();
         }
     }
 
     @Test(priority = 3) 
-    void volumeSlider() { //Req 6.4 & 6.5
+    void volumeSlider() throws InterruptedException { //Req 6.4 & 6.5
         try {
             driver.get("http://localhost:3000/library");
             driver.findElement(By.id("play-pause")).click(); //Play song
             Thread.sleep(3000);
             for(int i = 1; i < 20; i++) {
-                driver.findElement(By.id("volume")).sendKeys(Keys.ARROW_RIGHT) //Increase volume
+                driver.findElement(By.id("volume")).sendKeys(Keys.ARROW_RIGHT); //Increase volume
             }
             Thread.sleep(3000);
             Assert.assertTrue(true);
-        } catch {
-            Assert.assertTrue(false);
+        } catch(Exception e) {
+            Assert.fail();
         }
     }
 }
@@ -312,9 +311,9 @@ class Unit_7 {
             driver.get("http://localhost:3000/library");
             Select sortBox = new Select(driver.findElement(By.id("sort")));
             List<WebElement> selectedOptionList = sortBox.getAllSelectedOptions();
-            Assert.assertEqual(selectedOptionList, ["Title, Ascending"]);
-        } catch {
-            Assert.assertTrue(false);
+            Assert.assertEquals(selectedOptionList.get(0).getText(), "Title, Ascending");
+        } catch(Exception e) {
+            Assert.fail();
         }
     }
 
@@ -325,9 +324,9 @@ class Unit_7 {
             Select sortBox = new Select(driver.findElement(By.id("sort")));
             sortBox.selectByVisibleText("Title, Descending"); //Adjust the option selected
             List<WebElement> selectedOptionList = sortBox.getAllSelectedOptions();
-            Assert.assertEqual(selectedOptionList, ["Title, Descending"]); //Assert option is the only one selected
-        } catch {
-            Assert.assertTrue(false);
+            Assert.assertEquals(selectedOptionList.get(0).getText(), "Title, Descending"); //Assert option is the only one selected
+        } catch(Exception e) {
+            Assert.fail();
         }
     }
 
@@ -338,9 +337,9 @@ class Unit_7 {
             Select sortBox = new Select(driver.findElement(By.id("sort")));
             sortBox.selectByVisibleText("Artist, Ascending"); //Adjust the option selected
             List<WebElement> selectedOptionList = sortBox.getAllSelectedOptions();
-            Assert.assertEqual(selectedOptionList, ["Artist, Ascending"]); //Assert option is the only one selected
-        } catch {
-            Assert.assertTrue(false);
+            Assert.assertEquals(selectedOptionList.get(0).getText(), "Artist, Ascending"); //Assert option is the only one selected
+        } catch(Exception e) {
+            Assert.fail();
         }
     }
 }
