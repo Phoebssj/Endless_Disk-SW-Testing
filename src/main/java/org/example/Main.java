6b1360999415f775aa028de21653b961c6dcc45b
TrevorChess25 <tmchessnoe5123@eagle.fgcu.edu>
2024-04-20 16:25:06 -0400

    Refactored forward and rewind check functions to have less duplicate code. Removed unneeded arguments.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import net.bytebuddy.build.Plugin;
-import org.apache.commons.lang3.ObjectUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -82,6 +80,24 @@ public class Main {
             Thread.sleep(wait_time);
         }
 
+        public double get_current_time(WebElement playback_elem) {
+            String get_curr_time = "return arguments[0].currentTime;";
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            double current_time = (Double) js.executeScript(get_curr_time, playback_elem);
+            return current_time;
+        }
+
+        public double[] get_time_diff(WebElement playback_bar, String btn_id){
+            double[] time_diff = new double[2];
+            double initial_time = get_current_time(playback_bar);
+            time_diff[0] = initial_time;
+            // Find the seek button and click it
+            WebElement seek_btn = driver.findElement(By.id(btn_id));
+            double final_time = get_current_time(playback_bar);
+            time_diff[1] = final_time;
+            return time_diff;
+        }
+
         @Test
         public void test_pause() throws InterruptedException {
             String url = "http://localhost:3000/library";
@@ -101,40 +117,46 @@ public class Main {
             press_and_wait(pause_btn, 2000);
         }
 
-        public double get_current_time(WebElement playback_elem) {
-            String get_curr_time = "return arguments[0].currentTime;";
-            JavascriptExecutor js = (JavascriptExecutor) driver;
-            double current_time = (Double) js.executeScript(get_curr_time, playback_elem);
-            return current_time;
-        }
         @Test
-        public WebDriver forward_check(WebDriver driver, String url, String btn_id) {
+        public void forward_check() {
+            String url = "http://localhost:3000/library";
             driver.get(url);
-            String seek_forward = "Placeholder";
-            boolean seek_forward_worked = false;
-            // Find seek bar and check the initial time
+            String forward_id = "Placeholder";
+            boolean forward_worked = false;
+            // Find seek bar and get time change from seek forward
             WebElement playback_bar = driver.findElement(By.tagName("audio"));
-            double initial_time = get_current_time(driver, playback_bar);
-            // Find the seek button and click it
-            WebElement target_btn = driver.findElement(By.id(seek_forward));
-            target_btn.click();
-            // Use time diff to verify btn skips fwd 15 secs
-            double final_time = get_current_time(driver, playback_bar);
+            double[] time_diff = get_time_diff(playback_bar, forward_id);
+            double initial_time = time_diff[0];
+            double final_time = time_diff[1];
+            // Lag time added to account for loading or slow code execution
             double lag_time = 0.35;
-            // If endless disc has to load, or code takes more than lag_time to exec
-            // Test will still pass since the button still skips properly
-            if (final_time-initial_time < 15+lag_time && final_time-initial_time > 15-lag_time) {
-                seek_forward_worked = true;
+            // Final minus init time ensures result is pos
+            double time_skipped = final_time - initial_time;
+            if (time_skipped < 15+lag_time && time_skipped > 15-lag_time) {
+                forward_worked = true;
             }
-            Assert.assertTrue(seek_forward_worked);
-            return driver;
+            Assert.assertTrue(forward_worked);
         }
-        public WebDriver rewind_check(WebDriver driver, String url, String btn_id) {
-            return driver;
+        public void rewind_check() {
+            String url = "http://localhost:3000/library";
+            driver.get(url);
+            String backward_id = "Placeholder";
+            boolean backward_worked = false;
+            // Find seek bar and get time change from seek back
+            WebElement playback_bar = driver.findElement(By.tagName("audio"));
+            double[] time_diff = get_time_diff(playback_bar, backward_id);
+            double initial_time = time_diff[0];
+            double final_time = time_diff[1];
+            // Lag time added to account for loading or slow code execution
+            double lag_time = 0.35;
+            // Init minus final time ensures result is pos
+            double time_skipped = initial_time - final_time;
+            if (time_skipped < 15+lag_time && time_skipped > 15-lag_time) {
+                backward_worked = true;
+            }
+            Assert.assertTrue(backward_worked);
         }
     }
-
-
 
     class Unit_2 {
 
